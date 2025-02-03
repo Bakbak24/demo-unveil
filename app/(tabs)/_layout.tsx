7481a0309a704a5f3355aaeac501b22dcaa620a4
Bakbak24 <zakabak3@gmail.com>
2025-02-03 17:58:11 +0100

    feat: Enhance tab bar styling with shadow and elevation effects

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,57 +6,69 @@ export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarStyle: {
-          backgroundColor: "#212121",
-          borderTopColor: "#5CD4FF",
-          paddingTop: 17,
-          height: 90,
-        },
-        tabBarActiveTintColor: "#5CD4FF",
-        tabBarInactiveTintColor: "#FFFFFF",
-        tabBarLabelStyle: {
-          marginTop: 5,
-          fontSize: 14,
-        },
+      tabBarStyle: {
+        backgroundColor: "#212121",
+        borderTopWidth: 0,
+        paddingTop: 17,
+        height: 90,
+        boxShadow: "0px -4px 4px 0px rgba(0, 0, 0, 0.25)",
+        elevation: 10,
+      },
+      tabBarActiveTintColor: "#5CD4FF",
+      tabBarInactiveTintColor: "#FFFFFF",
+      tabBarLabelStyle: {
+        marginTop: 5,
+        fontSize: 14,
+      },
       }}
     >
       <Tabs.Screen
-        name="audio"
-        options={{
-          headerTitle: "Audio",
-          headerShown: false,
-          tabBarLabel: "Audio",
-          tabBarIcon: ({ color }) => (
-            <Ionicons
-              name="mic-outline"
-              size={32}
-              color={color}
-              marginTop={-1}
-            />
-          ),
-        }}
+      name="audio"
+      options={{
+        headerTitle: "Audio",
+        headerShown: false,
+        tabBarLabel: "Audio",
+        tabBarIcon: ({ color }) => (
+        <Ionicons
+          name="mic-outline"
+          size={32}
+          color={color}
+          marginTop={-1}
+        />
+        ),
+      }}
       />
       <Tabs.Screen
-        name="index"
-        options={{
-          headerTitle: "Map",
-          headerShown: false,
-          tabBarLabel: "Map",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="map-outline" size={32} color={color} marginTop={-2}/>
-          ),
-        }}
+      name="index"
+      options={{
+        headerTitle: "Map",
+        headerShown: false,
+        tabBarLabel: "Map",
+        tabBarIcon: ({ color }) => (
+        <Ionicons
+          name="map-outline"
+          size={32}
+          color={color}
+          marginTop={-2}
+        />
+        ),
+      }}
       />
       <Tabs.Screen
-        name="user"
-        options={{
-          headerTitle: "User",
-          headerShown: false,
-          tabBarLabel: "User",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person-outline" size={32} color={color} marginTop={-2}/>
-          ),
-        }}
+      name="user"
+      options={{
+        headerTitle: "User",
+        headerShown: false,
+        tabBarLabel: "User",
+        tabBarIcon: ({ color }) => (
+        <Ionicons
+          name="person-outline"
+          size={32}
+          color={color}
+          marginTop={-2}
+        />
+        ),
+      }}
       />
       <Tabs.Screen name="+not-found" options={{}} />
     </Tabs>
